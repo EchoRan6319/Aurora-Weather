cbfa361050e06c4efbdec172ad60586df2665f44
EchoRan <EchoRan6319@outlook.com>
2026-05-21 12:55:52 +0800

    fix: 更新 lucide_icons 导入路径，修复部分系统字体显示异常相关依赖问题；升级多个依赖版本

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:permission_handler/permission_handler.dart';
 import '../app_localizations.dart';
--- lib/widgets/hourly_forecast.dart
+++ lib/widgets/hourly_forecast.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'dart:ui';
